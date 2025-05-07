172b52f33103511dba0eff08765c8364beb5afc1
Jasper-Wesseling <wesselingjasper@hotmail.com>
2025-05-07 17:28:52 +0200

    added user registration via json request. hashing password, and preventing duplicate email on accounts

--- Backend/src/Controller/User/Controller.php
+++ Backend/src/Controller/User/Controller.php
@@ -2,18 +2,47 @@
 
 namespace App\Controller\User;
 
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
-final class Controller extends AbstractController
+#[Route('api/user')]
+class Controller extends AbstractController
 {
-    #[Route('/user/', name: 'app_user_')]
-    public function index(): JsonResponse
-    {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/User/Controller.php',
-        ]);
+    #[Route('/register', name: 'api_user_register', methods: ['POST'])]
+    public function register(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher ): Response {
+        $data = json_decode($request->getContent(), true);
+
+        if (!isset($data['email']) || !isset($data['password'])) {
+            return new JsonResponse(['error' => 'Missing email or password'], 400);
+        }
+
+        // Check if user with this email already exists
+        $existingUser = $entityManager->getRepository(User::class)->findOneBy(['email' => $data['email']]);
+        if ($existingUser) {
+            return new JsonResponse(['error' => 'Email already in use'], 409);
+        }
+
+        $user = new User();
+        $user->setEmail($data['email']);
+
+        $hashedPassword = $passwordHasher->hashPassword($user, $data['password']);
+        $user->setPassword($hashedPassword);
+
+        $entityManager->persist($user);
+        $entityManager->flush();
+
+        return new JsonResponse(['message' => 'User created'], 201);
+    }
+
+    #[Route('/login', name: 'api_user_login', methods: ['GET'])]
+    public function login(): Response {
+
+        return new JsonResponse(['message' => 'User'], 201);
     }
 }
