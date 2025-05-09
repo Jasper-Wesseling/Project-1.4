3c5864facfc5a97245bdf39275523caa45febc03
LukeDeAap <92108278+LukeDeAap@users.noreply.github.com>
2025-05-09 19:19:52 +0200

    voorbeeld react stack navigation en header

--- Frontend/StudentHub/App.js
+++ Frontend/StudentHub/App.js
@@ -1,8 +1,11 @@
-import { useState, useEffect } from 'react';
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StatusBar, Button, StyleSheet, Text, View, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+function HomeScreen({ navigation }) {
     const [data, setData] = useState(null);
     const [loading, setLoading] = useState(true);
   
@@ -21,9 +24,63 @@ export default function App() {
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
       <Text>{!loading ? data.message: 'loading...'}</Text>
-      <StatusBar style="auto" />
+      <Button 
+        title="Go to Details" 
+        onPress={() => navigation.navigate('Details')} // Navigate to DetailsScreen
+      />
     </View>
   );
+}
+
+function DetailsScreen({ route, navigation}) {
+  return (
+    <View style={styles.container}>
+      <Text>This is the {route.name} Screen!</Text>
+      <Button 
+        title="Go to Home" 
+        onPress={() => navigation.reset({
+          index: 0,
+          routes: [{ name: 'Home'}]
+        })} // Navigate to DetailsScreen
+      />
+    </View>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{
+          headerShown: true,
+        }}
+      >
+        <Stack.Screen 
+          name="Home" 
+          component={HomeScreen} 
+          options={{
+            headerStyle: { height: 150},
+            headerTitle: () => (
+              <View style={{ flexDirection: 'column', alignItems: 'center'}}>
+                  <Image 
+                    source={require('./assets/icon.png')} 
+                    style= {{ width : 60, height: 60 }}
+                  />
+                  <Text>moi</Text>
+              </View>
+            ),
+            headerLeft: () => (
+              <Button
+                title="Info"
+                onPress={() => alert('Info button pressed!')}
+                color="black"
+              />
+            ),
+          }}
+        />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  )
 }
 
 const styles = StyleSheet.create({
