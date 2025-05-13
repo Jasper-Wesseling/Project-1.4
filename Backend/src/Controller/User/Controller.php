24c88a29ca99d94ca8382443b9226eaad362674d
Jasper-Wesseling <wesselingjasper@hotmail.com>
2025-05-13 11:41:30 +0200

    user validation

--- Backend/src/Controller/User/Controller.php
+++ Backend/src/Controller/User/Controller.php
@@ -10,12 +10,13 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
 
 #[Route('api/user')]
 class Controller extends AbstractController
 {
     #[Route('/register', name: 'api_user_register', methods: ['POST'])]
-    public function register(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher ): Response {
+    public function register(Request $request, EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher, ValidatorInterface $validator, ): Response {
         $data = json_decode($request->getContent(), true);
 
         if (!isset($data['email']) || !isset($data['password'])) {
@@ -34,15 +35,30 @@ class Controller extends AbstractController
         $hashedPassword = $passwordHasher->hashPassword($user, $data['password']);
         $user->setPassword($hashedPassword);
 
+        $errors = $validator->validate($user);
+
+        if (count($errors) > 0) {
+            $violations = [];
+
+            foreach ($errors as $error) {
+                $violations[] = [
+                    'propertyPath' => $error->getPropertyPath(),
+                    'message' => $error->getMessage(),
+                ];
+            }
+
+            return new JsonResponse(['violations' => $violations], JsonResponse::HTTP_BAD_REQUEST);
+        }
+
         $entityManager->persist($user);
         $entityManager->flush();
 
         return new JsonResponse(['message' => 'User created'], 201);
     }
 
-    #[Route('/login', name: 'api_user_login', methods: ['GET'])]
+    #[Route('/test', name: 'api_user_test', methods: ['GET'])]
     public function login(): Response {
 
-        return new JsonResponse(['message' => 'User'], 201);
+        return new JsonResponse(['message' => 'test'], 201);
     }
 }
