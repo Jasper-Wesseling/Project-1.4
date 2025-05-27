727b06eb5a86e5d8c65a379e110dc946c327db98
AnneMaya <anne.maya.van.der.knaap@student.nhlstenden.com>
2025-05-27 09:42:12 +0200

    App js aangepast.

--- Frontend/StudentHub/App.js
+++ Frontend/StudentHub/App.js
@@ -1,6 +1,7 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Onboard from './components/Onboard';
+import Profile from './components/Profile';
 
 const Stack = createStackNavigator();
 export default function App() {
@@ -9,7 +10,7 @@ export default function App() {
       <Stack.Navigator screenOptions={{
           headerShown: false,
         }}>
-        <Stack.Screen name="Onboard" component={Onboard}/>
+        <Stack.Screen name="Onboard" component={Profile}/>
       </Stack.Navigator>
     </NavigationContainer>
   );
